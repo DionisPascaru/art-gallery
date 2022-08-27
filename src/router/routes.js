16536abfe29039102c4cfe773d1bc4be4131724b
DionisPascaru <denis.pascaru95@gmail.com>
2022-08-27 17:55:19 +0300

    Create all views, config routes related to new views components

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,33 @@
-import HomeView from '../views/HomeView';
+import GalleryView from '../views/GalleryView';
 import AboutView from '../views/AboutView';
+import EventsView from '../views/EventsView';
+import CommintSoonView from '../views/CommingSoonView';
+import ContactView from '../views/ContactView';
 
 export default [
     {
         path: '/',
-        name: 'HomeView',
-        component: HomeView
+        name: 'GalleryView',
+        component: GalleryView
+    },
+    {
+        path: '/events',
+        name: 'EventsView',
+        component: EventsView
     },
     {
         path: '/about',
         name: 'AboutView',
         component: AboutView
+    },
+    {
+        path: '/comming-soon',
+        name: 'CommingSoonView',
+        component: CommintSoonView
+    },
+    {
+        path: '/contact',
+        name: 'ContactView',
+        component: ContactView
     }
 ]
