d03a1010cd18fecb959bdd719083787c6a9b0c18
DionisPascaru <denis.pascaru95@gmail.com>
2022-09-24 12:08:13 +0300

    Change about page desing adjustments

---
+++ src/components/CopyrightComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="copyright container">
+        <span>all rights © 2022</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CopyrightComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+    .copyright {
+        border-top: 1px solid #8F0000;
+        padding: 3.125rem 0;
+
+        span {
+            font-size: 1.25rem;
+            color: #fff;
+        }
+    }
+</style>
