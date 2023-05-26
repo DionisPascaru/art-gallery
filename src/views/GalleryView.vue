2e3f310cb9e6bce24ba1b9d593dc03c8277586de
DionisPascaru <denis.pascaru95@gmail.com>
2023-05-27 01:25:54 +0300

    Optimize images from gallery.

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,18 +1,15 @@
 <template>
     <div>
-        <!-- <gallery-animation-component></gallery-animation-component> -->
         <gallery-component></gallery-component>
     </div>
 </template>
 
 <script>
-// import GalleryAnimationComponent from '../components/gallery/GalleryAnimationComponent.vue';
 import GalleryComponent from '../components/gallery/GalleryComponent.vue';
 
 export default {
     name: 'GalleryView',
     components: {
-        // GalleryAnimationComponent
         GalleryComponent
     }
 }
