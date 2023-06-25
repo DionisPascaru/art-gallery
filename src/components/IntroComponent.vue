57229f63ef947dce972d356e699582aff4756242
DionisPascaru <denis.pascaru95@gmail.com>
2023-06-25 19:20:52 +0300

    Add intro logo size fix.

--- src/components/IntroComponent.vue
+++ src/components/IntroComponent.vue
@@ -1,38 +1,39 @@
 <template>
-    <div class="intro-animation">
-        <img src="@/assets/logo.svg" alt="logo">
-    </div>
+  <div class="intro-animation">
+    <img src="@/assets/logo.svg" alt="logo">
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'IntroComponent'
+  name: 'IntroComponent'
 }
 </script>
 
 <style lang="scss" scoped>
 .intro-animation {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    img {
-        opacity: 0;
-        animation: fadeOpacity 3s ease-out;
-    }
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100px;
+    opacity: 0;
+    animation: fadeOpacity 3s ease-out;
+  }
 }
 
 @keyframes fadeOpacity {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
